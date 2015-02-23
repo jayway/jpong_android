cebe1bf1650631a278d27735c51d65bf5ca99e78
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2015-02-23 12:20:59 +0100

    nice japanses flag in pong acitivty, and some cleanup.

--- app/src/main/java/com/jayway/pong/PongActivity.java
+++ app/src/main/java/com/jayway/pong/PongActivity.java
@@ -4,33 +4,18 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
-
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 
 public class PongActivity extends ActionBarActivity {
-
-    //private WebSocketClient mWebSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(new MyView(this));
-
-        //connectWebSocket();
     }
 
     public class MyView extends View {
@@ -43,85 +28,19 @@ public class PongActivity extends ActionBarActivity {
         protected void onDraw(Canvas canvas) {
             // TODO Auto-generated method stub
             super.onDraw(canvas);
+            // Draw the flag of Japan!
+            // You might want to replace this with some paddles and a ball for pong...
             int x = getWidth();
             int y = getHeight();
             int radius;
-            radius = 100;
+            radius = (int) (Math.min(canvas.getWidth(), canvas.getHeight()) * (3f / 5f)) / 2;
             Paint paint = new Paint();
             paint.setStyle(Paint.Style.FILL);
             paint.setColor(Color.WHITE);
             canvas.drawPaint(paint);
-            // Use Color.parseColor to define HTML colors
-            paint.setColor(Color.parseColor("#CD5C5C"));
+            paint.setColor(0xffc70025);
             canvas.drawCircle(x / 2, y / 2, radius, paint);
         }
-    }
-
-//    private void connectWebSocket() {
-//        URI uri;
-//        try {
-//            uri = new URI("ws://:3000");
-//        } catch (URISyntaxException e) {
-//            e.printStackTrace();
-//            return;
-//        }
-//
-//        mWebSocketClient = new WebSocketClient(uri) {
-//            @Override
-//            public void onOpen(ServerHandshake serverHandshake) {
-//                Log.i("Websocket", "Opened");
-//                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
-//            }
-//
-//            @Override
-//            public void onMessage(String s) {
-//                final String message = s;
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        TextView textView = (TextView) findViewById(R.id.chat_text);
-//                        textView.setText(textView.getText() + "\n" + message);
-//                    }
-//                });
-//            }
-//
-//            @Override
-//            public void onClose(int i, String s, boolean b) {
-//                Log.i("Websocket", "Closed " + s);
-//            }
-//
-//            @Override
-//            public void onError(Exception e) {
-//                Log.i("Websocket", "Error " + e.getMessage());
-//            }
-//        };
-//        mWebSocketClient.connect();
-//    }
-//
-//    private void sendMessage(String message) {
-//       mWebSocketClient.send(message);
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_chat, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
