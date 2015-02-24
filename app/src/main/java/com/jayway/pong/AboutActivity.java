31089e56b22f8bcaaf94862a280ccdb84aa1078b
Max Ringström <max.ringstrom@jayway.com>
2015-02-24 16:20:48 +0100

    Added link to wikipedia on about page

--- app/src/main/java/com/jayway/pong/AboutActivity.java
+++ app/src/main/java/com/jayway/pong/AboutActivity.java
@@ -3,10 +3,12 @@ package com.jayway.pong;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class AboutActivity extends ActionBarActivity {
@@ -15,5 +17,8 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView aboutText = (TextView) findViewById(R.id.description);
+        aboutText.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
