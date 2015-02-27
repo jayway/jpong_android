688e1ca1fba9ee0c363f05d6b5219851997abcce
jennynilsson <jenny.nilsson@jayway.com>
2015-02-27 13:26:57 +0100

    First commit

--- app/src/main/java/com/jayway/pong/PongActivity.java
+++ app/src/main/java/com/jayway/pong/PongActivity.java
@@ -6,16 +6,95 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
 
 
 public class PongActivity extends ActionBarActivity {
+
+    private static final String TAG = PongActivity.class.getCanonicalName();
+    private Socket socket;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(new MyView(this));
+    }
+
+    private void connectWebSocket() {
+        Log.d(TAG, "connecting");
+        try {
+            socket = IO.socket("http://jaywaypongserver.herokuapp.com");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        socket.on(com.github.nkzawa.socketio.client.Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                Log.d(TAG, "connected: " + socket.connected());
+                addPlayer("Android#" + (int) (Math.random() * (1 << 10)));
+            }
+
+        }).on(com.github.nkzawa.socketio.client.Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                Log.d(TAG, "disconnected " + args);
+            }
+
+        }).on("players", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                Log.d(TAG, "players " + args);
+            }
+
+        }).on("message", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                if (args != null && args.length > 0 && args[0] instanceof JSONObject) {
+                    try {
+                        final String text = ((JSONObject) args[0]).getString("message");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuffer tv = new StringBuffer();
+                                tv.append(text);
+                                tv.append("\n");
+
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        socket.connect();
+    }
+
+    private void addPlayer(String playerName) {
+        if (playerName != null && playerName.length() > 0) {
+            JSONObject obj = null;
+            try {
+                obj = new JSONObject().put("playername", playerName);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            socket.emit("add player", obj);
+        }
     }
 
     public class MyView extends View {
@@ -42,5 +121,7 @@ public class PongActivity extends ActionBarActivity {
             canvas.drawCircle(x / 2, y / 2, radius, paint);
         }
     }
+
+
 
 }
---
+++ app/src/main/java/com/jayway/pong/model/Step.java
@@ -0,0 +1,21 @@
+package com.jayway.pong.model;
+
+
+public class Step {
+
+    /*
+    {
+        ball: {x:10, y: 300, x_speed: 3, y_speed: 5, radius:5},
+        playerPaddle: {x:106, y: 400, width:50, height:10},
+        remotePlayerPaddle: {x:100, y: 0,width:50, height:10},
+        players: {
+            player1: {name:"Albin", score:3},
+            player2: {name:"Christian", score:1}
+        },
+        bounds: {
+            width: 400,
+                    height: 600
+        }
+    }
+    */
+}
